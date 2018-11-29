faa882db33d0028f1a011946a7806f02f246e26f
Doppen <basdoppen@gmail.com>
2018-11-29 17:15:35 +0100

    bot dynamics

--- src/js/animation.js
+++ src/js/animation.js
@@ -3,11 +3,16 @@
 document.body.onkeyup = function(e){
   //spatie
   if(e.keyCode == 32){
-    execAnimation('c1big');
-    execAnimation('c2big');
+    //execAnimation('c1big');
+    //execAnimation('c2big');
     execAnimation('mouth-up-closed');
     execAnimation('mouth-un-closed');
     execAnimation('eyes-open');
+
+    execAnimation('cust', 'backCircle01', 'r', 500 );
+    execAnimation('cust', 'backCircle02', 'r', 400 );
+    execAnimation('cust', 'backCircle03', 'r', 300 );
+    execAnimation('cust', 'backCircle04', 'r', 600 );
 
   }
   // enter
@@ -16,7 +21,11 @@ document.body.onkeyup = function(e){
     execAnimation('mouth-un-closed');
 
   }
-
+  if(e.keyCode == 48){
+    execAnimation('mouth-up-swoosh');
+    execAnimation('mouth-un-swoosh');
+    execAnimation('eyes-shoosh');
+  }
   if(e.keyCode == 49){
     execAnimation('mouth-up-talk01');
     execAnimation('mouth-un-talk01');
@@ -30,8 +39,9 @@ document.body.onkeyup = function(e){
 
 
 
-var winking;
-winking = setInterval(eyesWink, 6400, 'eyes-wink');
+var winking, botDynamics;
+winking = setInterval(eyesWink, 6400);
+botDynamics = setInterval(dynamics, 1000);
 
 function eyesWink() {
   execAnimation('eyes-wink');
@@ -39,6 +49,11 @@ function eyesWink() {
   wink = setTimeout(execAnimation, 100, 'eyes-open');
 }
 
+function dynamics() {
+  var elems=['backCircle01','backCircle02','backCircle03','backCircle04'];
+  var randElems = elems[Math.floor(Math.random() * elems.length)];
+  execAnimation('cust', randElems, 'r', getRandomNum(200, 700) );
+}
 
 
 
@@ -49,12 +64,22 @@ function eyesWink() {
 
 
 // get data
-function execAnimation(animationName) {
-  for (var i = 0; i < animationValues.length; i++){
-    if (animationValues[i].name == animationName){
-      document.getElementById(animationValues[i].id).setAttribute(animationValues[i].attribute, animationValues[i].code);
-    }
+function execAnimation(animationName, id, attr, code) {
+
+  if (animationName == 'cust') {
+    document.getElementById(id).setAttribute(attr, code);
   }
+  else {
+    for (var i = 0; i < animationValues.length; i++){
+      if (animationValues[i].name == animationName){
+        document.getElementById(animationValues[i].id).setAttribute(animationValues[i].attribute, animationValues[i].code);
+      }
+    }
+
+  }
+
 }
 
-//execAnimation('eyes-wink');
+function getRandomNum(min, max) {
+    return Math.random() * (max - min) + min;
+}

2a2986c064fee1e815a3eea68fe11caa3d8d65fc
Bas Doppen <basdoppen@gmail.com>
2018-11-29 14:02:10 +0100

    eyes wink

--- _dist/scripts/combined.js
+++ _dist/scripts/combined.js
@@ -9,17 +9,11 @@ var animationValues = [
     "name": "eyes-open",
     "id": "botEyes",
     "attribute": "d",
-    "code": "M2,179.7c10.6,42.8,130.5,75.7,130.5,75.7c12.4,5.8,22.3,6.4,38.7,4.7c0,0,30.8,8.3,69.8-0.3c14.8,1.4,26.3,1.1,38.6-4.4c38.8-4.8,91.4-28,122.4-77.7"
+    "code": "M2,179.7c10.6,42.8,130.5,75.7,130.5,75.7c7.7-29.1,43.1-12.9,38.7,4.7c0,0,30.8,8.3,69.8-0.3c-0.6-22.3,29.8-26.4,38.6-4.4c38.8-4.8,91.4-28,122.4-77.7"
   },
 ];
 
-function anni_eyes_wink() {
-  document.getElementById('botEyes').setAttribute("d", "M2,179.7c10.6,42.8,130.5,75.7,130.5,75.7c12.4,5.8,22.3,6.4,38.7,4.7c0,0,30.8,8.3,69.8-0.3c14.8,1.4,26.3,1.1,38.6-4.4c38.8-4.8,91.4-28,122.4-77.7");
-}
 
-function anni_eyes_Open() {
-  document.getElementById('botEyes').setAttribute("d", "M2,179.7c10.6,42.8,130.5,75.7,130.5,75.7c12.4,5.8,22.3,6.4,38.7,4.7c0,0,30.8,8.3,69.8-0.3c14.8,1.4,26.3,1.1,38.6-4.4c38.8-4.8,91.4-28,122.4-77.7");
-}
 
 document.body.onkeyup = function(e){
   if(e.keyCode == 32){
@@ -37,21 +31,32 @@ document.body.onkeyup = function(e){
 
 
 
-var myVar;
+var winking;
+winking = setInterval(eyesWink, 3000, 'eyes-wink');
 
-function myFunction() {
-    //myVar = setInterval(alertFunc, 3000);
+function eyesWink() {
+  execAnimation('eyes-wink');
+  var wink;
+  wink = setTimeout(execAnimation, 100, 'eyes-open');
 }
+
+
+
+
+
+
+
+
+
+
 
 function execAnimation(animationName) {
   for (var i = 0; i < animationValues.length; i++){
-    // look for the entry with a matching `code` value
     if (animationValues[i].name == animationName){
 
-      // animationValues[i].name is the matched result
       document.getElementById(animationValues[i].id).setAttribute(animationValues[i].attribute, animationValues[i].code);
     }
   }
 }
 
-execAnimation('eyes-wink');
+//execAnimation('eyes-wink');
--- src/js/animation.js
+++ src/js/animation.js
@@ -1,10 +1,4 @@
-function anni_eyes_wink() {
-  document.getElementById('botEyes').setAttribute("d", "M2,179.7c10.6,42.8,130.5,75.7,130.5,75.7c12.4,5.8,22.3,6.4,38.7,4.7c0,0,30.8,8.3,69.8-0.3c14.8,1.4,26.3,1.1,38.6-4.4c38.8-4.8,91.4-28,122.4-77.7");
-}
 
-function anni_eyes_Open() {
-  document.getElementById('botEyes').setAttribute("d", "M2,179.7c10.6,42.8,130.5,75.7,130.5,75.7c12.4,5.8,22.3,6.4,38.7,4.7c0,0,30.8,8.3,69.8-0.3c14.8,1.4,26.3,1.1,38.6-4.4c38.8-4.8,91.4-28,122.4-77.7");
-}
 
 document.body.onkeyup = function(e){
   if(e.keyCode == 32){
@@ -22,21 +16,32 @@ document.body.onkeyup = function(e){
 
 
 
-var myVar;
+var winking;
+winking = setInterval(eyesWink, 3000, 'eyes-wink');
 
-function myFunction() {
-    //myVar = setInterval(alertFunc, 3000);
+function eyesWink() {
+  execAnimation('eyes-wink');
+  var wink;
+  wink = setTimeout(execAnimation, 100, 'eyes-open');
 }
+
+
+
+
+
+
+
+
+
+
 
 function execAnimation(animationName) {
   for (var i = 0; i < animationValues.length; i++){
-    // look for the entry with a matching `code` value
     if (animationValues[i].name == animationName){
 
-      // animationValues[i].name is the matched result
       document.getElementById(animationValues[i].id).setAttribute(animationValues[i].attribute, animationValues[i].code);
     }
   }
 }
 
-execAnimation('eyes-wink');
+//execAnimation('eyes-wink');

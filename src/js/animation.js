1086305fb07cdec9168af7645fb63a99f612faf1
Bas Doppen <basdoppen@gmail.com>
2018-11-29 15:25:01 +0100

    swoosh state

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,23 +1,37 @@
 
 
 document.body.onkeyup = function(e){
+  //spatie
   if(e.keyCode == 32){
-      anni_eyes_wink();
-  }
+    execAnimation('c1big');
+    execAnimation('c2big');
+    execAnimation('mouth-up-closed');
+    execAnimation('mouth-un-closed');
+    execAnimation('eyes-open');
 
+  }
+  // enter
   if(e.keyCode == 13){
+    execAnimation('mouth-up-closed');
+    execAnimation('mouth-un-closed');
 
   }
 
-  if(e.keyCode == 191){
-
+  if(e.keyCode == 49){
+    execAnimation('mouth-up-talk01');
+    execAnimation('mouth-un-talk01');
   }
+  if(e.keyCode == 50){
+    execAnimation('mouth-up-talk02');
+    execAnimation('mouth-un-talk02');
+  }
+
 }
 
 
 
 var winking;
-winking = setInterval(eyesWink, 3000, 'eyes-wink');
+winking = setInterval(eyesWink, 6400, 'eyes-wink');
 
 function eyesWink() {
   execAnimation('eyes-wink');
@@ -34,11 +48,10 @@ function eyesWink() {
 
 
 
-
+// get data
 function execAnimation(animationName) {
   for (var i = 0; i < animationValues.length; i++){
     if (animationValues[i].name == animationName){
-
       document.getElementById(animationValues[i].id).setAttribute(animationValues[i].attribute, animationValues[i].code);
     }
   }
